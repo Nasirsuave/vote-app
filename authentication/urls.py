6da20e1f6ac0850a4ae62095e48c5980bd6b0209
nasirsuave <safarinabanku@gmail.com>
2024-12-02 17:51:52 +0000

    authentication pashe I

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', login, name='login'),
+    path('', login_page, name='login'),
+    path('login/',login_page,name='login'),
+    path('register/',register_page,name='register')
 ]
